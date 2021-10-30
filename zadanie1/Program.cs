b6c429a6ce8a2bc170a5a91f2d132b96afee4d9a
Jakub Cz <92373216+JakubCzyzUE@users.noreply.github.com>
2021-10-30 16:23:19 +0200

    Full project

---
+++ zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 14.5;
+            double b = 24.45;
+
+            Console.Write("{0}" +"\n"+
+               "{1}",a,b);
+            Console.ReadLine();
+        }
+    }
+}
